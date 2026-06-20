b1b1800673aed48758e4eb1eb5e969f1e537f39e
Jérôme Vogel <jerome.vogel@gmail.com>
2026-06-19 22:35:54 -0400

    0.6.3-alpha: localise the /about page label
    Page labels are _($lid) (gettext) -> rdfs:label, so an untranslated lid renders
    verbatim. Added `msgid "about"` -> "About" (en_EN) / "À propos" (fr_FR) to the
    gettext catalogs and recompiled the .mo; the page name now localises in the
    header, sitemap and breadcrumb. Verified EN/FR live. en_US left as-is (unused).

--- luna/luna.php
+++ luna/luna.php
@@ -61,7 +61,7 @@ class luna {
 	 * @access	public
 	 * @var		string
 	 */
-	public static $lunaVersion = '0.6.2-alpha';
+	public static $lunaVersion = '0.6.3-alpha';
 	/**
 	 * instance
 	 * @var object
